2329954392291cffbdfc6cf075936ed928674f82
Peter Marinov <peter.marinov@progress.com>
2016-10-12 12:32:03 +0300

    Adding the navigation model

---
+++ WebApp/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using SitefinityWebApp.Mvc.Models;
+using System;
+using System.Web;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Frontend;
+using Telerik.Sitefinity.Frontend.Navigation.Mvc.Models;
+
+namespace SitefinityWebApp
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Initialized += this.Bootstrapper_Initialized;
+        }
+
+        private void Bootstrapper_Initialized(object sender, ExecutedEventArgs e)
+        {
+            if (e.CommandName == "Bootstrapped")
+            {
+                FrontendModule.Current.DependencyResolver.Rebind<INavigationModel>().To<CustomNavigationModel>();
+            }
+        }
+    }
+}
---
+++ WebApp/Mvc/ViewModels/CustomNodeViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Web;
+using Telerik.Sitefinity.Frontend.Navigation.Mvc.Models;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.ViewModels
+{
+    public class CustomNodeViewModel : NodeViewModel
+    {
+        public CustomNodeViewModel() : base()
+        {
+            this.rating = this.GetRating();
+        }
+
+        public CustomNodeViewModel(SiteMapNode node, string url, string target, bool isCurrentlyOpened, bool hasChildOpen)
+            : base(node, url, target, isCurrentlyOpened, hasChildOpen)
+        {
+            this.rating = this.GetRating();
+        }
+
+        public int Rating
+        {
+            get
+            {
+                return this.rating;
+            }
+        }
+
+        private int GetRating()
+        {
+            var node = this.OriginalSiteMapNode as PageSiteNode;
+            var rating = node.GetCustomFieldValue("Rating");
+
+            return (rating != null) ? Decimal.ToInt32((decimal)rating) : 0;
+        }
+
+        private readonly int rating;
+    }
+}
